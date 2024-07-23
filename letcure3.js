1b810cb1a5b70d6ab9cd581bcc0432b63336da9c
Aman <amancodingblocks@outlook.com>
2024-07-23 08:11:29 -0700

    JS-Lec-05

--- letcure3.js
+++ letcure3.js
@@ -1,82 +1,135 @@
-// // // const arr = [1,2,3,4]
-// // // // for(let i=0;i<arr.length;i++){
-// // // //     console.log(arr[i])
+
+// // // // // // // // // // debugger
+// // // // // // // // // // console.log(b)
+// // // // // // // // // var a= 100;
+// // // // // // // // // let b =200;
+// // // // // // // // // // console.log(a,b)
+// // // // // // // // // var fun =()=>{
+// // // // // // // // //     var c=10000;
+// // // // // // // // //     let d=200000;
+// // // // // // // // //     console.log("Function declaration")
+// // // // // // // // // }
+
+// // // // // // // // // fun();
+
+// // // // // // // // // console.log(c);
+
+
+// // // // // // // // //higher order functions
+
+// // // // // // // // function callback(){
+// // // // // // // //     console.log("I  am callback")
+// // // // // // // // }
+
+// // // // // // // // function higherOrder(cb=callback){
+// // // // // // // //     // cb();
+// // // // // // // //     return cb;
+// // // // // // // // }
+
+// // // // // // // // const result = higherOrder()
+// // // // // // // // result()
+// // // // // // // // //closures
+
+// // // // // // // debugger
+// // // // // // // function a(){
+// // // // // // // let b = 100;
+// // // // // // // return function(){
+// // // // // // //     let a=200;
+// // // // // // //     console.log(a+b)
+// // // // // // // }
+// // // // // // // }
+// // // // // // // const result= a();
+// // // // // // // result();
+
+// // // // // // //Asynchronous JS
+
+// // // // // // console.log("Start")
+// // // // // // let count =0;
+
+// // // // // // const id = setInterval(()=>{
+// // // // // //     console.log("delay")
+// // // // // //     count++;
+// // // // // //     if(count == 2) clearInterval(id)
+
+// // // // // // },1000)
+
+// // // // // // console.log("end")
+
+// // // // // // function fun(){
+// // // // // //   console.log("fun")
+// // // // // // }
+
+// // // // // // fun();
+
+
+// // // // // //Promises
+
+
+// // // // // // pending resolved rejected
+
+// // // // // setTimeout(()=>{
+// // // // // console.log("time out")
+// // // // // },0)
+
+// // // // // const promise = new Promise((resolve,reject)=>{
+// // // // //     setTimeout(()=>{
+// // // // //       resolve("I am Resolved")
+// // // // //       reject("I am Rejected")
+// // // // //     },2000)
+// // // // // })
+
+// // // // // promise.then((res)=>{
+// // // // //     console.log(res)
+// // // // // })
+// // // // // .catch(err=>{
+// // // // //     console.log(err)
+// // // // // })
+
+// // // // // fetch('https://dummyjson.com/quo')
+// // // // // .then(res =>{
+// // // // //   // console.log(res)
+// // // // //   return res.json()
+// // // // // }).then(response=>{
+// // // // //   console.log(response)
+// // // // // }).catch(err=>{
+// // // // //   console.log("I am error")
+// // // // // })
+
+// // // // // Async await 
+
+// // // // async function asyncFun(){
+// // // // const data = await fetch('https://dummyjson.com/quotes')
+// // // // const result = await data.json();
+// // // // console.log(result)
 // // // // }
-// // // // For of Loop
-// // // // for(let value of arr){
-// // // //     console.log(value)
-// // // // }
 
-// // // // For each loop
+// // // // asyncFun()
 
-// // // // arr.forEach(function(value,index){
-// // // //     console.log(value,index)
-// // // // })
+// // // console.log("abc"==1)
 
+// // // Destructuring
 
-// // // // Map Method
+// // const arr = [10,20,30,40]
+// // const [a,b=100,c,...res]= [10,,30,40,50,60]
+// // console.log(a,b,c,res)
 
-// // // // const result= arr.map(function(value,index,array){
-// // // //     return value*100
-// // // // })
-// // // // filter Method
-// // // // const result = arr.filter(function(value,index,array){
-// // // //     return (value%2==0)
-// // // // })
+// const arr = [1,2,3,4,5]
+// const brr =[...arr,100,200,..."abc"]
+// console.log(brr)
 
-// // // // reduce Method
-
-// // // const result = arr.reduce(function(acc,curr,array){
-// // //     console.log(acc,curr )
-// // //     return acc.concat(curr+100)
-// // // },[])
-
-
-// // // console.log( result,typeof result)
-
-
-// // //objects
-
-// // var person = {
-// //     name:"jack",
-// //     age:45,
-// //     city:"ny",
-// //     citizen:true,
-// //     age:32
-// // }
-// // function newKey (){
-// //     return "name"
-// // }
-
-// // person.homeAddress="address"
-// // // console.log(person[newKey()])
-
-
-// // //Loops in Objects
-
-// // for( let key in person){
-// //     console.log(key,person[key])
-// // }
-
-// // console.log(Object.keys(person))
-// // console.log(Object.values(person))
-
-// const set = new Set()
-// set.add(2);
-// set.add(2);
-// set.add(3)
-// console.log(set);
-
-
-//functions
-
-function add(a,b){
-    return a+b    //function declaration
+const person = {
+  name:"weekend",
+  age:"30"
 }
+// const {name:a,age:b,city="nyc"} = person
+// console.log(city,a,b)
+
+const singer = {...person, city:"Delhi"}
+console.log(singer)
 
 
-const subtract = function(a,b){
-    return a-b;  //function expression
-}
 
-const multiply = (a,b)=> a*b   // arrow functions
+
+
+
+
